e8e5011fc35cf92a86ae0d672cdb70785b2498fb
ali-131586N <ali-131586n@ali-4c32758b20db.local>
2018-05-14 20:22:50 +0800

    add logrus

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import (
+    log "github.com/sirupsen/logrus"
+)
 
 func main(){
     fmt.Println("Hello, Golang!")
+    log.WithFields(log.Fields{
+        "animal": "walrus",
+    }).Info("A walrus appears")
 }
